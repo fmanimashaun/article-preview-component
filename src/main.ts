12358b94cfb04f08ff169e4d5df4d24459fe851d
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-11-13 16:33:37 +0100

    Update card structure and style

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './assets/scss/main.scss'
+
+const shareBtn = document.querySelectorAll('.card__share-btn') as NodeListOf<HTMLButtonElement>;
+const shareContainer = document.querySelector('.card__content-social') as HTMLDivElement;
+
+shareBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    shareContainer.classList.toggle('active');
+  });
+});
